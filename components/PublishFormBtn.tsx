3e302707368e8cab5262096fed24e3b4eb1945e1
WYZ00 <thanthtettin@gmail.com>
2023-10-03 23:16:43 +0630

    designing builder(not finished yet)

---
+++ components/PublishFormBtn.tsx
@@ -0,0 +1,15 @@
+import { MdOutlinePublic } from "react-icons/md";
+
+import { Button } from "./ui/button";
+
+function PublishFormBtn() {
+  return (
+    <Button
+      variant={"outline"}
+      className="gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400">
+      <MdOutlinePublic className="h-4 w-4" />
+      Publish
+    </Button>
+  );
+}
+export default PublishFormBtn;
